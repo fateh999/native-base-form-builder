d06b279fbf832df09672532a409d901cac6806b9
Fateh Farooqui <farooquifateh184@gmail.com>
2021-07-18 21:19:24 +0530

    Update InputText.tsx

--- src/Inputs/InputText.tsx
+++ src/Inputs/InputText.tsx
@@ -1,24 +1,12 @@
 import {FormControl, Input, ScrollView} from 'native-base';
-import React, {useMemo} from 'react';
-import {StyleSheet} from 'react-native';
+import React from 'react';
 import {InputTextProps} from '../Types/Types';
 
 function InputText(props: InputTextProps) {
   const {formState, field, textInputProps, CustomTextInput, label, isRequired} =
     props;
   const errorMessage = formState.errors?.[field.name]?.message;
-  const textColor = errorMessage ? 'red' : undefined;
   const INPUT = CustomTextInput ?? Input;
-
-  const styles = useMemo(
-    () =>
-      StyleSheet.create({
-        textInputStyle: {
-          color: textColor,
-        },
-      }),
-    [textColor],
-  );
 
   return (
     <FormControl isRequired={isRequired} isInvalid={errorMessage}>
@@ -44,7 +32,6 @@ function InputText(props: InputTextProps) {
         ref={field.ref}
         value={field.value}
         onChangeText={(text: string) => field.onChange(text)}
-        style={[styles.textInputStyle, textInputProps?.style]}
       />
       {errorMessage && (
         <ScrollView
